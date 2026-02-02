0f4450ce74dbdd5483ddc713eb0b0b6eda015330
Kaz-gif <thechurroman360@gmail.com>
2026-02-01 20:31:13 -0600

    feat: (test) first script

---
+++ MyProject/Assets/Scripts/test.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Welcome");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello world");
+    }
+}
